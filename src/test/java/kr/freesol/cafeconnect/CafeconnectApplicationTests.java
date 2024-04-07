212449d3956cfe5341d86a679d9108eb17c76192
3ltigr0 <jjparksw@gmail.com>
2024-04-07 21:53:29 +0900

    init: Initialize Spring Boot project

---
+++ src/test/java/kr/freesol/cafeconnect/CafeconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.freesol.cafeconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
